e833c3e71ebadd563d8d20ab6a2a6af9293d44ca
roschel <joaomroschel@gmail.com>
2024-07-19 00:22:49 -0300

    Input search

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,31 @@
-import { Container } from "./styles";
+import logo from "../../assets/logo.svg";
+import TextField, { Input } from "@material/react-text-field";
+
+import { Container, Search } from "./styles";
+import { useState } from "react";
 
 const Home = () => {
-  return <Container>Hello World</Container>;
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const inputTarget = e.target as HTMLInputElement;
+    setInputValue(inputTarget.value);
+  };
+
+  return (
+    <Container>
+      <Search>
+        <img src={logo} alt="Logo do restaurante" />
+        <TextField
+          label="Pesquisar"
+          outlined
+          // onTrailingIconSelect={() => this.setState({ value: "" })}
+          // trailingIcon={<MaterialIcon role="button" icon="delete" />}
+        >
+          <Input value={inputValue} onChange={handleInputChange} />
+        </TextField>
+      </Search>
+    </Container>
+  );
 };
 export { Home };
