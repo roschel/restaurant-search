f443861c7c698c28a86a20b14f9eea73e5f958ed
roschel <joaomroschel@gmail.com>
2024-07-18 23:52:31 -0300

    home page e reset estilos

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,6 @@
+import { Container } from "./styles";
+
+const Home = () => {
+  return <Container>Hello World</Container>;
+};
+export { Home };
---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'styled-components';
+const theme:DefaultTheme = {
+  colors: {
+    primary: "#6200ee",
+    background: "#00000014",
+    text: "#000000B3",
+    success: "#135e91",
+  },
+  fonts: { regular: "Roboto, sans-serif" },
+};
+
+export default theme;
